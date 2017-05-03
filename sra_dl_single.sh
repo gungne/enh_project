cc5a90c5dcc813b0b0570b93cfee6e55067fb269
gungne <zhxzcj@gmail.com>
2017-05-03 00:08:30 -0400

    update

--- sra_dl_single.sh
+++ sra_dl_single.sh
@@ -12,7 +12,10 @@ then
 else
 	fastq-dump -A $1 -O $HOME/enh_project/CAP_SELEX/ 
 	echo "create $dir$1.fastq"
+	# wc -l "$dir$1.fastq"
 	fastq_to_fasta -v -Q33 -i "$dir$1.fastq" -o "$dir$1.fa"
 	rm "$dir$1.fastq"
+	# echo '$1.fa' >>'fasta_summary.txt'
+	wc -l "$dir$1.fa" >> 'fasta_summary.txt'
 fi
 
